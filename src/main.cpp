767041462add3569cadb392562f49b7ec8f06af4
josecorrearosa1026-maker <josecorrearosa1026@gmail.com>
2026-05-17 16:37:34 -0500

    Movimiento basico del jugador

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Dungeon Game");
-
+sf::RectangleShape jugador({40, 40});
+jugador.setFillColor(sf::Color::Green);
+jugador.setPosition({100, 100});
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -12,9 +14,29 @@ int main()
             {
                 window.close();
             }
-        }
+        }if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+{
+    jugador.move({0, -0.2f});
+}
 
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+{
+    jugador.move({0, 0.2f});
+}
+
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+{
+    jugador.move({-0.2f, 0});
+}
+
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+{
+    jugador.move({0.2f, 0});
+}
         window.clear();
+
+        window.draw(jugador);
+
         window.display();
     }
 
